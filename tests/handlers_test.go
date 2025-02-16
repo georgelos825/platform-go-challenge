0198fc861b8f3948c683c47dfe883e833bd5f009
georgelos825 <georgelos007@hotmail.com>
2025-02-16 10:07:16 +0200

    Refined and updated API tests, added user initialization for seamless runs, and aligned test payloads with the latest API structure.

--- tests/handlers_test.go
+++ tests/handlers_test.go
@@ -5,8 +5,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"platform-go-challenge/models"
 	"platform-go-challenge/routes"
+	"platform-go-challenge/storage"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -22,6 +22,8 @@ var (
 func init() {
 	router = routes.SetupRouter()
 	testToken = getTestToken()
+	// Add a test user manually to storage
+	storage.InitializeTestUser("123")
 }
 
 // getTestToken retrieves a JWT token only once
@@ -38,14 +40,16 @@ func getTestToken() string {
 
 // Test adding a favorite asset
 func TestAddFavorite(t *testing.T) {
-	reqBody := models.Asset{
-		ID:          "1",
-		Type:        models.ChartType,
-		Description: "Stock trends",
-	}
 	requestData := map[string]interface{}{
 		"user_id": "123",
-		"asset":   reqBody,
+		"asset": map[string]interface{}{
+			"id":          "1",
+			"type":        "chart",
+			"description": "Stock trends",
+			"title":       "Tech Market Overview",
+			"axes_titles": []string{"Time", "Price"},
+			"data":        []int{3500, 3600},
+		},
 	}
 	jsonValue, _ := json.Marshal(requestData)
 	req, _ := http.NewRequest("POST", "/favorites", bytes.NewBuffer(jsonValue))
@@ -64,15 +68,7 @@ func TestGetFavorites(t *testing.T) {
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
-}
-
-// Test removing a favorite
-func TestRemoveFavorite(t *testing.T) {
-	req, _ := http.NewRequest("DELETE", "/favorites/123/1", nil)
-	req.Header.Set("Authorization", "Bearer "+testToken)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), "Stock trends")
 }
 
 // Test editing a favorite's description
@@ -86,4 +82,14 @@ func TestEditFavorite(t *testing.T) {
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Contains(t, w.Body.String(), "Asset updated")
+}
+
+// Test removing a favorite
+func TestRemoveFavorite(t *testing.T) {
+	req, _ := http.NewRequest("DELETE", "/favorites/123/1", nil)
+	req.Header.Set("Authorization", "Bearer "+testToken)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Contains(t, w.Body.String(), "Asset removed")
 }
